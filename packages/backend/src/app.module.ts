4f954bbddf64829f9578adec63e759c4c0a68279
yun8boo <yuma051521@gmail.com>
2022-02-10 09:19:35 +0900

    log

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { PrismaModule } from 'nestjs-prisma';
 import { join } from 'path';
 import { RecipesModule } from './recipes/recipes.module';
 import { AuthModule } from './auth/auth.module';
@@ -9,11 +10,18 @@ import { CurrentUserModule } from './current-user/current-user.module';
 import { PasswordService } from './password/password.service';
 import { PostsModule } from './posts/posts.module';
 import { RecipeTagsModule } from './recipe-tags/recipe-tags.module';
+import { loggingMiddleware } from './logging/logging.middleware';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    PrismaModule.forRoot({
+      isGlobal: true,
+      prismaServiceOptions: {
+        middlewares: [loggingMiddleware()],
+      },
     }),
     GraphQLModule.forRoot({
       debug: true,
