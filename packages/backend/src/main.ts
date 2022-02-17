59a65cc7309135ea9847df2876198a651a2f2ecc
yun8boo <yuma051521@gmail.com>
2022-02-17 21:49:10 +0900

    commit

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CustomLogger } from './logger/custom-logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new CustomLogger()
+  });
   await app.listen(5000);
 }
 bootstrap();
