ec64cf81e474c115071df518df7687f298f299f1
yun8boo <yuma051521@gmail.com>
2022-02-21 23:33:22 +0900

    commit

--- packages/backend/src/recipes/recipes.service.spec.ts
+++ packages/backend/src/recipes/recipes.service.spec.ts
@@ -3,17 +3,17 @@ import { RecipesModule } from './recipes.module';
 import { RecipesService } from './recipes.service';
 
 describe('RecipesService', () => {
-  let service: RecipesService;
+  let recipeService: RecipesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [RecipesModule],
     }).compile();
 
-    service = module.get<RecipesService>(RecipesService);
+    recipeService = module.get<RecipesService>(RecipesService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(recipeService).toBeDefined();
   });
 });
